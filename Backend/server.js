c43a2a7831720e2e0cccd5d650b6741ed806a00a
Sathiyaseelan97 <sathiyanselvam97.ss@gmail.com>
2025-11-20 15:45:32 +0530

    server js and package json modified

--- Backend/server.js
+++ Backend/server.js
@@ -2,21 +2,21 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import router from "./routes/route.js";
-import db from "./db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*", // allow all (Netlify + Railway)
     credentials: true,
   })
 );
+
 app.use(bodyParser.json());
 
 app.use("/api", router);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
